ddcd8eebf3049be5ba79d2501fc4c4d76f443fb5
Elana Kopelevich <ekopelevich@gmail.com>
2015-11-19 08:50:01 -0700

    added content, fixed easing issue

--- javascript/main.js
+++ javascript/main.js
@@ -8,6 +8,17 @@ $(window).scroll(function() {
     }
 });
 
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$( function() {
+    $( 'a.page-scroll' ).bind( 'click', function( event ) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
 // Close responsive menu on menu item click
 $( '.navbar-collapse ul li a' ).click( function() {
     $( '.navbar-toggle:visible' ).click();
